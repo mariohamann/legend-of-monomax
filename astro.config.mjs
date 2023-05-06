fb6bf2a1c44def7571f9730e6752e9e509382d28
Mario Hamann <mariovhamann@gmail.com>
2023-05-06 22:13:32 +0200

    chore: add tailwind and updacte packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import alpinejs from "@astrojs/alpinejs";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
+  integrations: [alpinejs(), tailwind()]
 });
